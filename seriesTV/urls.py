38414dcc1d51321efcd28a45651d9f1548381f31
Cornejo <acornejolopez@safareyes.es>
2022-05-09 00:15:21 +0200

    Eliminar por Id en construccion

--- seriesTV/urls.py
+++ seriesTV/urls.py
@@ -9,6 +9,9 @@ from .views import *
 urlpatterns = [
     path('', mostrar_inicio),
     path('seriesTV/crear_serie', crear_serie),
-    path('seriesTV/borrar_serie', borrar_serie)
+    path('seriesTV/borrar_serie', borrar_serie),
+    path('seriesTV/ver_serie/<id>', ver_serie),
+    path('seriesTV/modificar_serie/<id>', modificar_serie)
+
 
 ]
--- seriesTV/views.py
+++ seriesTV/views.py
@@ -20,11 +20,39 @@ def crear_serie(request):
         serie_nueva.anio = request.POST.get('anio')
         Serie.save(serie_nueva)
         return redirect("/seriesTV")
+
+def modificar_serie(request,id):
+    if request.method == 'GET':
+        modificar_serie = Serie.objects.get(id=id)
+        return render(request, 'modificar_serie.html', {"serie":modificar_serie})
+    else:
+        modificar_serie = Serie.objects.get(id=id)
+        modificar_serie.nombre = request.POST.get ('nombre')
+        modificar_serie.genero = request.POST.get ('genero')
+        modificar_serie.plataforma = request.POST.get('plataforma')
+        modificar_serie.capitulos = request.POST.get('capitulos')
+        modificar_serie.anio = request.POST.get('anio')
+        Serie.save(modificar_serie)
+        return redirect("/seriesTV")
 def borrar_serie(request):
     if request.method == 'GET':
         return render(request, 'borrar_serie.html')
     else:
         serie_vieja = Serie()
-        serie_vieja.nombre = request.POST.get('nombre')
+        serie_vieja.id = request.POST.get('id')
         Serie.delete(serie_vieja)
         return redirect("/seriesTV")
+
+def ver_serie(request, id):
+    if request.method == 'GET':
+        ver_serie = Serie.objects.get(id=id)
+        return render(request, 'ver_serie.html', {"serie":ver_serie})
+    else:
+        ver_serie = Serie.objects.get(id=id)
+        ver_serie.nombre = request.POST.get ('nombre')
+        ver_serie.genero = request.POST.get ('genero')
+        ver_serie.plataforma = request.POST.get('plataforma')
+        ver_serie.capitulos = request.POST.get('capitulos')
+        ver_serie.anio = request.POST.get('anio')
+        return redirect("/seriesTV")
+
